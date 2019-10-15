e77aece61744de3d59e1d4beca19b1ddfbba2dfe
Ari Crellin-Quick <a.crellinquick@gmail.com>
2019-10-15 10:59:24 -0700

    Add git commit date and hash to skyportal version number if dev version

--- skyportal/__init__.py
+++ skyportal/__init__.py
@@ -1,1 +1,16 @@
 __version__ = '0.9.dev0'
+
+if 'dev' in __version__:
+    import subprocess
+    p = subprocess.Popen(['git', '--version'], stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if 'git version' in out.decode('utf-8'):
+        p = subprocess.Popen(['git', 'log', '-1', '--format="%h %aI"'],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        git_hash, git_date = (out.decode('utf-8').strip().replace('"', '')
+                              .split('T')[0].replace('-', '').split())
+
+        __version__ += f'+git{git_date}.{git_hash}'
